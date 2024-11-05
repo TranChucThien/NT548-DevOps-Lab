9a8c6afb2714f4fa93a6a0cd0c6e6f0a60e91c35
Trần Chức Thiện <102866314+TranChucThien@users.noreply.github.com>
2024-11-05 22:54:42 +0700

    Update main.tf

--- Lab_1/main.tf
+++ Lab_1/main.tf
@@ -9,7 +9,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files = [ "C:/Users/chuct/.aws/credentials" ]
+  #shared_credentials_files = [ "C:/Users/chuct/.aws/credentials" ]
 }
 
 
