ded80ac8cd3b244098492466ada0dd3c68703c15
Trần Chức Thiện <102866314+TranChucThien@users.noreply.github.com>
2024-11-13 01:10:29 +0700

    Update main.tf

--- Lab_1/main.tf
+++ Lab_1/main.tf
@@ -6,15 +6,16 @@ terraform {
     }
   }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  #shared_credentials_files = [ "C:/Users/chuct/.aws/credentials" ]
 }
+
 
 ##############################  VPC ##############################
 
-# Create VPC
+# Tạo VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support = true
@@ -25,76 +26,36 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Enable VPC flow logging
-resource "aws_flow_log" "vpc_flow_log" {
-  log_destination_type = "cloud-watch-logs"
-  vpc_id               = aws_vpc.main.id
-  traffic_type         = "ALL"
-
-  log_group_name = "/aws/vpc/flow-logs"
-  iam_role_arn   = var.iam_role_arn # Ensure an IAM role for VPC Flow Logs
-}
-
-############################  Subnets ############################
-
-# Create public subnet
+# Tạo public subnet
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
-  map_public_ip_on_launch = false  # Ensures no public IP assigned by default
-  depends_on              = [aws_vpc.main]
-  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  depends_on = [ aws_vpc.main ]
   tags = {
     Name = "${var.vpc_name}-public"
   }
 }
 
-# Create private subnet
+# Tạo private subnet
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidr
-  depends_on = [aws_vpc.main]
-  
+  depends_on = [ aws_vpc.main ]
   tags = {
     Name = "${var.vpc_name}-private"
   }
 }
 
-# Restrict all traffic in default security group
+# Tạo default security group
 resource "aws_security_group" "default" {
   vpc_id = aws_vpc.main.id
-  depends_on = [aws_vpc.main]
-
-  # No ingress or egress rules to block all traffic
-
-  tags = {
-    Name = "${var.vpc_name}-default-sg"
-  }
-}
-
-# Create Internet Gateway for public subnet
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  depends_on = [aws_vpc.main]
-
-  tags = {
-    Name = "${var.vpc_name}-igw"
-  }
-}
-
-########################  Security Groups ########################
-
-# Public EC2 Security Group
-resource "aws_security_group" "public_ec2_sg" {
-  vpc_id = aws_vpc.main.id
-  depends_on = [aws_vpc.main]
-
+  depends_on = [ aws_vpc.main ]
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.public_ip]
-    description = "Allow SSH access from specified IP for public instances"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -102,7 +63,100 @@ resource "aws_security_group" "public_ec2_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic for public instances"
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-default-sg"
+  }
+}
+
+# Tạo Internet Gateway cho public subnet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  depends_on = [ aws_vpc.main ]
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# Tạo EIP & NAT Gateway cho private subnet
+resource "aws_eip" "nat" {
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id    = aws_subnet.public.id
+  depends_on    = [aws_subnet.public, aws_eip.nat]
+
+  tags = {
+    Name = "${var.vpc_name}-nat"
+  }
+}
+
+##########################  Route Table ##########################
+
+# Tạo Route Table cho public subnet, route tới Internet Gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  depends_on = [ aws_internet_gateway.igw ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+# Liên kết Route Table với public subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+  depends_on = [ aws_subnet.public, aws_route_table.public ]
+}
+
+# Tạo Route Table cho private subnet, route tới NAT Gateway
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  depends_on = [ aws_nat_gateway.nat ]
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+# Liên kết Route Table với private subnet
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+  depends_on = [ aws_subnet.private, aws_route_table.private ]
+}
+
+########################  Security Groups ########################
+
+resource "aws_security_group" "public_ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  depends_on = [ aws_vpc.main ]
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.public_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -110,25 +164,25 @@ resource "aws_security_group" "public_ec2_sg" {
   }
 }
 
-# Private EC2 Security Group
 resource "aws_security_group" "private_ec2_sg" {
   vpc_id = aws_vpc.main.id
-  depends_on = [aws_vpc.main]
+
+  depends_on = [ aws_vpc.main, aws_subnet.public, aws_security_group.public_ec2_sg ]
 
   ingress {
+    cidr_blocks = [aws_subnet.public.cidr_block]
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.public.cidr_block]
-    description = "Allow SSH access from public subnet for private instances"
+    security_groups = [aws_security_group.public_ec2_sg.id]
   }
 
   ingress {
+    cidr_blocks = [aws_subnet.public.cidr_block]
     from_port   = 23
     to_port     = 23
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.public.cidr_block]
-    description = "Allow custom port access from public subnet for private instances"
+    security_groups = [aws_security_group.public_ec2_sg.id]
   }
 
   egress {
@@ -136,7 +190,6 @@ resource "aws_security_group" "private_ec2_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic for private instances"
   }
 
   tags = {
@@ -147,25 +200,13 @@ resource "aws_security_group" "private_ec2_sg" {
 ##############################  EC2 ##############################
 
 resource "aws_instance" "public_instance" {
-  depends_on = [aws_security_group.public_ec2_sg, aws_subnet.public]
+  depends_on = [ aws_security_group.public_ec2_sg, aws_subnet.public ]
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name      = "public-ec2-key"
-  ebs_optimized = true # Ensures EBS optimization
-  monitoring    = true # Enable detailed monitoring
-
+  key_name = "public-ec2-key"
+  
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.public_ec2_sg.id]
-
-  iam_instance_profile = aws_iam_instance_profile.public_ec2_role.name # Attach IAM role for security
-
-  metadata_options {
-    http_tokens = "required"
-  }
-
-  root_block_device {
-    encrypted = true # Ensures root volume is encrypted
-  }
 
   tags = {
     Name = "${var.vpc_name}-public-instance"
@@ -173,26 +214,14 @@ resource "aws_instance" "public_instance" {
 }
 
 resource "aws_instance" "private_instance" {
-  depends_on = [aws_security_group.private_ec2_sg, aws_subnet.public]
+  depends_on = [ aws_security_group.private_ec2_sg, aws_subnet.public ]
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name      = "private-ec2-key"
-  ebs_optimized = true # Ensures EBS optimization
-  monitoring    = true # Enable detailed monitoring
-
+  key_name = "private-ec2-key"
+  
   subnet_id     = aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.private_ec2_sg.id]
-
-  iam_instance_profile = aws_iam_instance_profile.private_ec2_role.name # Attach IAM role for security
-
-  metadata_options {
-    http_tokens = "required"
-  }
-
-  root_block_device {
-    encrypted = true # Ensures root volume is encrypted
-  }
-
+  vpc_security_group_ids = [ aws_security_group.private_ec2_sg.id ]
+  
   tags = {
     Name = "${var.vpc_name}-private-instance"
   }
